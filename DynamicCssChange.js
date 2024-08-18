67096e31314dbb3bab10a7349562f8cb79437e72
Ismail Hossain <155625863+ismai402@users.noreply.github.com>
2024-08-18 22:53:39 +0600

    Add files via upload

---
+++ DynamicCssChange.js
@@ -0,0 +1,10 @@
+var myVar = document.querySelector("#paraId");
+function addStyle() {
+    myVar.classList.add("para-style");
+
+}
+
+function removeStyle() {
+    myVar.classList.remove("para-style");
+
+}
